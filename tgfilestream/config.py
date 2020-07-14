f2649f8563ee53c5f9d246385e9f4826a51dadf4
rockmeup <38371598+rockmeup@users.noreply.github.com>
2020-07-14 10:24:06 +0530

    Update config.py

--- tgfilestream/config.py
+++ tgfilestream/config.py
@@ -28,7 +28,7 @@ if not 1 <= port <= 65535:
 
 try:
     api_id = int(os.environ[1624982])
-    api_hash = os.environ["87a4b1e4f1f27983548f11393c7ed26a"]
+    api_hash = os.environ[87a4b1e4f1f27983548f11393c7ed26a]
 except (KeyError, ValueError):
     print("Please set the TG_API_ID and TG_API_HASH environment variables correctly")
     print("You can get your own API keys at https://my.telegram.org/apps")
